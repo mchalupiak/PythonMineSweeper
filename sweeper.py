02280cb384597a11f7ff969e48223457aa0d0a27
namespace-miata <54217969+namespace-miata@users.noreply.github.com>
2021-05-22 20:04:50 -0400

    First commit

---
+++ sweeper.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def main():	
+
+get_board_size():
+	max_width = os.get_terminal_size()[0]
+	max_height = os.get_terminal_size()[1]
+
+	while not(set):
+		width = input("Enter your desired board width: ")
+def board(width, height):
+	sweeper_board = [[] * width] * height
+
+if __name__ == "__main__":
+	main()
