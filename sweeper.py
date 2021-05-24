3f0bd725b1a9f9e71afe25e84734547d1cc7f0ae
namespace-miata <54217969+namespace-miata@users.noreply.github.com>
2021-05-23 21:49:31 -0400

    Making progress...

--- sweeper.py
+++ sweeper.py
@@ -1,16 +1,28 @@
-import sys
 import os
 
+
+class Board:
+
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.board = [['X'] * self.width] * self.height
+	
+	def print_board(self):
+		for i in range(self.width):
+			for j in range(self.height):
+				print(self.board[i][j], end=' ')
+			print('')
+		print('')
+
+	def update_board(self, pos_x, pos_y):
+		self.board[pos_x][pos_y] = 'w'
+
 def main():	
-
-get_board_size():
-	max_width = os.get_terminal_size()[0]
-	max_height = os.get_terminal_size()[1]
-
-	while not(set):
-		width = input("Enter your desired board width: ")
-def board(width, height):
-	sweeper_board = [[] * width] * height
+	mine_sweeper = Board(7, 7)
+	mine_sweeper.print_board()
+	mine_sweeper.update_board(4, 3)
+	mine_sweeper.print_board()
 
 if __name__ == "__main__":
 	main()
