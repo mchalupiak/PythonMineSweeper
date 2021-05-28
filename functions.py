0066f8baffc0ab3eff16cce02de7350d61f6aa02
namespace-miata <54217969+namespace-miata@users.noreply.github.com>
2021-05-28 08:02:05 -0400

    Working on gettings things functional

--- functions.py
+++ functions.py
@@ -4,14 +4,12 @@ from board_class import Board
 def add_mines(num_of_mines, board):
     used_x = []
     used_y = []
-    i = 0
-    while i < num_of_mines:
+    i = 1
+    while i <= num_of_mines:
         x_pos = randint(0, board.width - 1)
         y_pos = randint(0, board.height - 1)
-        if not(x_pos in used_x and y_pos in used_y):
+        if not(x_pos in used_x) and not(y_pos == used_y[used_x.index(x_pos)]):
             board.update_board(x_pos, y_pos, '\033[91mM\033[0m') 
             used_x.append(x_pos)
             used_y.append(y_pos)
-            i += 1
-        else:
-            continue
+        i += 1
--- sweeper.py
+++ sweeper.py
@@ -6,7 +6,7 @@ from functions import add_mines
 def main():	
     mine_sweeper = Board(7, 7)
     mine_sweeper.print_board()
-    add_mines(12, mine_sweeper)
+    add_mines(14, mine_sweeper)
     mine_sweeper.print_board()
 
 if __name__ == "__main__":
