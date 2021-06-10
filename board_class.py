77ac33e406c8537e9197915ac3d67cbb1e809ff8
namespace-miata <54217969+namespace-miata@users.noreply.github.com>
2021-06-10 07:36:14 -0400

    Fixed 'Update Mines' function and moved it to Board_Class

--- board_class.py
+++ board_class.py
@@ -1,3 +1,4 @@
+from random import randint
 # This class define the entire game board
 # It will be used to create two boards, one for storing what the player sees and one for storing the underlying mines and mine numbers around a given block
 # I'm new to classes in python (or anywhere else), so this file will be heavily commented
@@ -19,3 +20,16 @@ class Board:
 
     def update_board(self, pos_x, pos_y, char):
         self.board[pos_x][pos_y] = char
+
+    def add_mines(self, num_of_mines):
+        mines_remaining = num_of_mines
+        running = True
+        mine = "\033[91mM\033[0m"
+        while running:
+            for j in range(self.width):
+                for k in range(self.height):
+                    if mines_remaining == 0:
+                        running = False
+                    elif self.board[j][k] != mine and randint(1, self.width * self.height) == 1:
+                        mines_remaining -= 1
+                        self.board[j][k] = mine
--- sweeper.py
+++ sweeper.py
@@ -1,12 +1,9 @@
 from board_class import Board
-import os
-from board_class import Board
-from functions import add_mines
 
 def main():	
     mine_sweeper = Board(7, 7)
     mine_sweeper.print_board()
-    add_mines(14, mine_sweeper)
+    mine_sweeper.add_mines(5)
     mine_sweeper.print_board()
 
 if __name__ == "__main__":
