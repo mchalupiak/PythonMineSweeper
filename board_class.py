dc7c7bcd177810f3bc1ad7976c58f2501a5fc1f1
namespace-miata <mikecchalupiak@outlook.com>
2021-08-03 10:55:49 -0400

    Implemented space select functionality

--- board_class.py
+++ board_class.py
@@ -7,34 +7,31 @@ class Board:
     # This function defines parameters when an instance of the class is created. Here we give it a width and height for the number of positions
     # Later on I will add logic to prevent it from exceeding the terminal window width and height
     # board_mask is what the player will see, and board is used to store things like where the mines are and how many mines area around a given point
-    def __init__(self, width, height):
+    def __init__(self, width, height, num_of_mines):
         self.width = width
         self.height = height
         self.board_mask = [['X'] * self.width for _ in range(self.height)]
         self.board = [[0] * self.width for _ in range(self.height)]
         self.mine = "\033[91mM\033[0m"
-
-    # Just a handy helper function to print the board out when I need it
-    def print_board(self):
-        for i in range(self.width):
-            for j in range(self.height):
-                print(self.board[j][i], end=' ')
-            print('')
-        print('')
-
-    # add_mines adds mines at a random positions across the board
-    # Mines are red, even though in hindsight the player won't see the mines
-    def add_mines(self, num_of_mines):
-        mines_remaining = num_of_mines
+        self.num_of_mines = num_of_mines
+        self.num_of_open_spaces = (self.width * self.height) - num_of_mines
+        self.game_over = False
         running = True
         while running:
             for j in range(self.width):
                 for k in range(self.height):
-                    if mines_remaining == 0:
+                    if num_of_mines == 0:
                         running = False
                     elif self.board[k][j] != self.mine and randint(1, self.width * self.height) == 1:
-                        mines_remaining -= 1
+                        num_of_mines -= 1
                         self.board[k][j] = self.mine
+    # Just a handy helper function to print the board out when I need it
+    def print_board(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                print(self.board_mask[j][i], end=' ')
+            print('')
+        print('')
 
     def is_val(self, x_pos, y_pos, val):
         try:
@@ -49,6 +46,9 @@ class Board:
         except IndexError:
             return False
 
+    def end_screen(self, did_win):
+        self.game_over = True
+
     def configure_board(self, x_pos, y_pos):
         if not self.is_val(x_pos, y_pos, self.mine):
             surrounding_spaces = [[x_pos - 1, y_pos - 1], [x_pos, y_pos - 1], [x_pos + 1, y_pos - 1], [x_pos - 1, y_pos], [x_pos + 1, y_pos], [x_pos - 1, y_pos + 1], [x_pos, y_pos + 1], [x_pos + 1, y_pos + 1]]
@@ -58,3 +58,13 @@ class Board:
             for j in range(len(mines_around)):
                 if mines_around[j] == True:
                     self.board[y_pos][x_pos] += 1
+
+    def make_move(self, x_pos, y_pos):
+        self.board_mask[y_pos][x_pos] = self.board[y_pos][x_pos]
+
+        if self.num_of_open_spaces <= 0:
+            self.end_screen(True)
+        if self.is_val(x_pos, y_pos, self.mine):
+            self.end_screen(False)
+
+        self.num_of_open_spaces -= 1
--- sweeper.py
+++ sweeper.py
@@ -5,14 +5,17 @@ def main():
     width = int(os.get_terminal_size()[0] / 2)
     height = os.get_terminal_size()[1] - 2
     num_of_mines = int(width * height / 4.84)
-    mine_sweeper = Board(height, width)
+    mine_sweeper = Board(height, width, num_of_mines)
     mine_sweeper.print_board()
-    mine_sweeper.add_mines(num_of_mines)
     mine_sweeper.print_board()
     for i in range(width):
         for j in range(height):
             mine_sweeper.configure_board(j, i)
     mine_sweeper.print_board()
-    input("Print something")
+    while mine_sweeper.game_over == False:
+        x_pos = input("Enter an X coordinate: ")
+        y_pos = input("Enter a Y coordinate: ")
+        mine_sweeper.make_move(int(x_pos), int(y_pos))
+        mine_sweeper.print_board()
 if __name__ == "__main__":
     main()
